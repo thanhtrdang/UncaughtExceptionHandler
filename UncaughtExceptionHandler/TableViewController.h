eecc6c302013008158a8b5ebc92d4ce5ffd4a7f9
thanhtrdang <thanhtrdang@gmail.com>
2015-03-20 12:01:48 +0700

    Fork www.cocoawithlove.com/2010/05/handling-unhandled-exceptions-and.html

---
+++ UncaughtExceptionHandler/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  UncaughtExceptionHandler
+//
+//  Created by Thanh Dang on 3/20/15.
+//  Copyright (c) 2015 Cocoa with Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
